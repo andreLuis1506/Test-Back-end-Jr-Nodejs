0cb0a25c9633f7861d6b1d13902cdc30c0fbd82c
Andre Luis dos Santos <alsss.ads@gmail.com>
2021-06-02 18:46:37 -0300

    feat: :hammer: adiciona validação de email e senha

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,14 +1,18 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
 
 @Entity('Users')
 export default class User {
     @PrimaryGeneratedColumn('uuid')
     id: string; 
 
-    @Column()
+    @Column({unique: true})
+    @IsEmail()
     email: string
 
     @Column()
+    @MaxLength(20, {message: 'O tamanho maximo a senha precisa ser de 20 caracteres'})
+    @MinLength(6, {message: 'O tamanho minimo a senha precisa ser de 6 caracteres'})
     senha: string
 
     @CreateDateColumn({name: 'created_at'})
